eb8a2f492d3663dc1eb07dbb1a6bf6c4d3ffeaab
shailja026 <shailjagupta4466@gmail.com>
2022-10-12 18:34:56 +0530

    ass2

---
+++ ass2/prob3.js
@@ -0,0 +1,11 @@
+
+//Problem 3: Given 2 numbers a and b print which is greater or "both equal".
+let a=10;
+let b=29;
+if(a>b){
+  console.log("greater",a)
+} else if (b>a){
+  console.log("greater",b)
+}else{
+  console.log("both equal")
+}
---
+++ ass2/prob4.js
@@ -0,0 +1,18 @@
+//
+/*PROBLEM 4
+Problem 4: Given stored username and password and input username and password, Print if the user can login or not.*/
+//for_database
+let data ="shailja192";
+let pass ="shail@132";
+//for login
+let username = "shailja192";
+let mypass = "shai@321";
+if(data==username){
+  if(pass==mypass){
+    console.log("login")
+  }else{
+    console.log("can't login")
+  }
+}else{
+  console.log("can't login")
+}
